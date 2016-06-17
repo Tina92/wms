5864f9e999f970adb75c93d1b8a76a0e3f75ae71
jw <jw@htnm.com>
2016-06-17 17:16:15 +0800

    order

--- WMS/Theme/Ticket/Default/Order/Order_index.php
+++ WMS/Theme/Ticket/Default/Order/Order_index.php
@@ -1,2 +1,188 @@
-
-this is body!
+<link rel="stylesheet" href="<?= DOCUMENT_ROOT; ?>/Theme/assets/css/sass/sass/order.css">
+<div class="am-g ">
+        <div class="am-u-sm-6">
+        <div class="am-panel am-panel-primary">
+            <div class="am-panel-hd">新提交工单</div>
+            <ul class="am-list">
+                <li class="am-cf">
+            		<div class="am-u-sm-3">
+            			申请人
+            		</div>
+            		<div class="am-u-sm-3">
+            			申请类型
+            		</div>
+            		<div class="am-u-sm-3">
+            			申请时间
+            		</div>
+            		<div class="am-u-sm-3">
+            			操作
+            		</div>
+                </li>
+                <li class="am-cf">
+            		<div class="am-u-sm-3">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-3">
+            			<a>设计</a>
+            		</div>
+            		<div class="am-u-sm-3">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-3">
+            			<a  class="am-btn am-btn-primary">通过</a>
+            			<a  class="am-btn am-btn-danger">不通过</a>
+            		</div>
+                </li>
+                <li class="am-cf">
+            		<div class="am-u-sm-3">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-3">
+            			<a>设计</a>
+            		</div>
+            		<div class="am-u-sm-3">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-3">
+            			<a  class="am-btn am-btn-primary">通过</a>
+            			<a  class="am-btn am-btn-danger">不通过</a>
+            		</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+        <div class="am-u-sm-6">
+        <div class="am-panel am-panel-warning">
+            <div class="am-panel-hd">进行中的工单</div>
+            <ul class="am-list">
+                <li class="am-cf">
+            		<div class="am-u-sm-2">
+            			申请人
+            		</div>
+            		<div class="am-u-sm-2">
+            			申请类型
+            		</div>
+            		<div class="am-u-sm-2">
+            			分配时间
+            		</div>
+            		<div class="am-u-sm-2">
+            			预计完成时间
+            		</div>
+            		<div class="am-u-sm-2">
+            			执行人
+            		</div>
+            		<div class="am-u-sm-2">
+            			操作
+            		</div>
+                </li>
+                <li class="am-cf">
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>设计</a>
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>重新选择</a>
+            		</div>
+                </li>
+                <li class="am-cf">
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>设计</a>
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>重新选择</a>
+            		</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+        <div class="am-u-sm-12">
+        <div class="am-panel am-panel-success">
+            <div class="am-panel-hd">已完成工单</div>
+            <ul class="am-list">
+                <li class="am-cf">
+            		<div class="am-u-sm-2">
+            			申请人
+            		</div>
+            		<div class="am-u-sm-2">
+            			申请类型
+            		</div>
+            		<div class="am-u-sm-2">
+            			申请时间
+            		</div>
+            		<div class="am-u-sm-2">
+            			完成时间
+            		</div>
+            		<div class="am-u-sm-2">
+            			执行人
+            		</div>
+            		<div class="am-u-sm-2">
+            			操作
+            		</div>
+                </li>
+                <li class="am-cf">
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>设计</a>
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>删除</a>
+            		</div>
+                </li>
+                 <li class="am-cf">
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>设计</a>
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			2016-06-17
+            		</div>
+            		<div class="am-u-sm-2">
+            			姜炜
+            		</div>
+            		<div class="am-u-sm-2">
+            			<a>删除</a>
+            		</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
